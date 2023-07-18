9e10e04eeea9c45f0967c46826fb259e45eb69fa
Njål Sørland <njaalis2000@gmail.com>
2023-07-18 21:08:49 +0200

    refactor colors const to utils

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,18 @@
 mod modules {
     pub mod banner;
     pub mod texture_buffer;
+    pub mod utils;
 }
 
 use modules::{
     banner::Banner,
     texture_buffer::TextureBuffer,
+    utils::COLORS,
 };
+
 use image::Rgba;
 use rand::Rng;
 
-const COLORS: [(Rgba<u8>, &str); 16] = [
-    (Rgba([25, 25, 25, 255]), "Black"),
-    (Rgba([76, 76, 76, 255]), "Dark Grey"),
-    (Rgba([153, 153, 153, 255]), "Grey"),
-    (Rgba([255, 255, 255, 255]), "White"),
-    (Rgba([242, 127, 165, 255]), "Pink"),
-    (Rgba([178, 76, 216, 255]), "Magenta"),
-    (Rgba([127, 63, 178, 255]), "Purple"),
-    (Rgba([51, 76, 178, 255]), "Blue"),
-    (Rgba([76, 127, 153, 255]), "Cyan"),
-    (Rgba([102, 153, 216, 255]), "Light Blue"),
-    (Rgba([102, 127, 51, 255]), "Green"),
-    (Rgba([127, 204, 25, 255]), "Lime"),
-    (Rgba([229, 229, 51, 255]), "Yellow"),
-    (Rgba([216, 127, 51, 255]), "Orange"),
-    (Rgba([102, 76, 51, 255]), "Brown"),
-    (Rgba([153, 51, 51, 255]), "Red"),
-];
 
 fn main() {
     let texture_buffer = TextureBuffer::new("./src/textures");
